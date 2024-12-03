ddec3df7c59ef33df081421f06b9cef3b92ad14c
cmill25 <coltonm2525@gmail.com>
2024-12-02 21:52:20 -0600

    messing around with getting variants

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from src.api.client import get_data
 import json
 
 def main():
-    data = get_data('users')
-
-    print(json.dumps(data, indent=4))
+    data = get_data('products')
+    
+    product_names = []
+    for i in data['data']:
+        metadata = {
+            'name': i['name'],
+            'has_variants': i['has_variants'],
+            'variant_name': i['variant_name']
+        }
+        product_names.append(metadata)
+    
+    print(json.dumps(product_names, indent=4))
 
 main()
+
+#prob just want to grab all the names and scids
